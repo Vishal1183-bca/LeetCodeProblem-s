237acdac8771b357791008d222392c34710c2c46
Vishal-1334 <vishalbhaliya1334@gmail.com>
2024-12-31 10:58:39 +0530

    LeetCode Problem 35 Search Insertion Position Solution

---
+++ SearchInsertPosition_35.java
@@ -0,0 +1,30 @@
+//Vishal Bhaliya
+//31-12-2024
+
+public class SearchInsertPosition_35
+{
+    public int searchInsert(int[] nums, int target) 
+    {
+        int start = 0; 
+        int end = nums.length -1;
+        while(start <= end)
+        {
+            int mid = (start + end) / 2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[mid] < target)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid -1;
+            }
+        }
+        return start;
+    }
+
+    
+}
